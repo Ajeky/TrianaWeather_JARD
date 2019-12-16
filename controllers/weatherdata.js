17191f3c888e2c12d1d061d49ba969e25c3cc7e5
Daniel Leiva Nieto <xleiiva@gmail.com>
2019-12-16 19:56:01 +0100

    Implementados 3 endpoints de gestión de datos

--- controllers/weatherdata.js
+++ controllers/weatherdata.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const _ = require('lodash');
+//const error_types = require('./error_types');
 const WeatherData = require('../models/weatherdata')
 
 module.exports = {
-
-    nuevoWeatherData : (req,res) => {
+    //Método que sirve para crear un WeatherData(datos meteorologicos)
+    createWeatherData : (req,res) => {
 
         let weatherData = new WeatherData({
             lluvia : req.body.lluvia,
@@ -14,12 +15,44 @@ module.exports = {
             temperarura : req.body.temperarura,
             humedad : req.body.humedad,
             calidad_aire : req.body.calidad_aire,
-            presion : req.body.presion
+            presion : req.body.presion,
+            fecha : req.body.fecha
         });
 
         weatherData.save()
             //populate estacion .then(wd => )
             .then(wd => res.status(201).json(wd))
             .catch(err => res.send(500).json(err.message))
+    },
+    //Método para obtener un WeatherData por su id
+    getWeatherData : (req,res,next) => {
+        const weatherData_id = req.params.id
+        WeatherData.findById(weatherData_id)
+            .exec()
+            .then(doc => res.status(200).json(doc))
+            .catch(err => res.send(404).json(err.message))
+    },
+    //Método para traer todos los WeatherData de hoy
+    getWeatherDataToday : (req,res) => {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+
+        var end = new Date();
+        end.setHours(23,59,59,999);
+
+        WeatherData.find({fecha:{$gte: start, $lt: end}})
+            .exec()
+            .then(docs => res.status(200).json(docs))
+            .catch(err => res.send(500).json(err.message))
+    },
+    getWeatherDataFromTo : (req,res) => {
+        console.log('fromto');
+        console.log(req.params.from);
+        console.log('to');
+        console.log(req.params.to);
+        
+        
+        
     }
+
 }
--- routes/weatherdata.js
+++ routes/weatherdata.js
@@ -4,6 +4,9 @@ const express = require('express')
 const router = express.Router()
 const WeatherDataController = require('../controllers/weatherdata')
 
-router.post('/',WeatherDataController.nuevoWeatherData)
+router.post('/',WeatherDataController.createWeatherData)
+router.get('/today',WeatherDataController.getWeatherDataToday)
+router.get('/:id',WeatherDataController.getWeatherData)
+router.get('/from/:from/to/:to',WeatherDataController.getWeatherDataFromTo)
 
 module.exports = router
