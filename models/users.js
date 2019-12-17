6902ea8d151d8f14b9d2fc24ad8e407ebd7467ea
Ajeky <alvaromarquez98@gmail.com>
2019-12-17 12:06:31 +0100

    Creado modelo de usuario

---
+++ models/users.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    nombre_completo: String,
+    rol: { type: String, enum: ["USER", "MANAGER", "ADMIN"] },
+    fecha_creacion: { type: Date, default: Date.now },
+    password: String,
+    estaciones_registradas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Estaciones' }],
+    estaciones_mantenidas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Estaciones' }]
+});
+
+module.exports = mongoose.model('User', userSchema);
