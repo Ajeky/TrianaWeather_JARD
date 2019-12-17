8c3e59a84f3c29750f38a1ff68212ef9762895ee
Ajeky <alvaromarquez98@gmail.com>
2019-12-17 14:28:50 +0100

    Controlador de usuario casi terminado

---
+++ controllers/user.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const error_types = require('./error_types');
+const User = require('../models/users');
+
+let controller = {
+
+    register: (req, res, next) => {
+        User.find({ username: req.body.username }, (err, result) => {
+            if (result.length > 0) next(new error_types.Error400("El usuario ya existe"));
+            else {
+                let pass = bcrypt.hashSync(req.body.password, parseInt(process.env.BCRYPT_ROUNDS));
+                let user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    nombre_completo: req.body.nombre_completo,
+                    rol: req.body.rol,
+                    fecha_creacion: Date.now,
+                    password: pass,
+                    estaciones_registradas: [],
+                    estaciones_mantenidas: []
+                });
+
+                user.save((err, user) => {
+                    if (err) next(new error_types.Error400(err.message));
+                    res.status(201).json(user);
+                });
+            }
+        });
+    },
+
+    login: (req, res, next) => {
+        passport.authenticate("local", { session: false }, (error, user) => {
+            if (error || !user) next(new error_types.Error404("El usuario o la contraseña son incorrectos"))
+            else {
+                const payload = {
+                    sub: user.id,
+                    exp: Date.now() + parseInt(process.env.JWT_LIFETIME),
+                    username: user.username
+                };
+                const token = jwt.sign(JSON.stringify(payload), process.env.JWT_SECRET, { algorithm: process.env.JWT_ALGORITHM });
+                res.json({
+                    username: user.username,
+                    token: token
+                });
+            }
+        })(req, res)
+    )
+}
+}
--- models/users.js
+++ models/users.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     username: String,
     email: String,
     nombre_completo: String,
-    rol: { type: String, enum: ["USER", "MANAGER", "ADMIN"] },
+    rol: { type: String, enum: ["USER", "MANAGER", "ADMIN"], default: "USER" },
     fecha_creacion: { type: Date, default: Date.now },
     password: String,
     estaciones_registradas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Estaciones' }],
