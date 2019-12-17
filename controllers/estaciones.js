f7d6916e3a6ea7ae4b3efcb5bdc4b56d11dda6c3
Daniel Leiva Nieto <dleiva@AULAS.TRIANA>
2019-12-17 11:56:17 +0100

    Commit sin apenas contenido actualizar esta rama con master

--- controllers/estaciones.js
+++ controllers/estaciones.js
@@ -10,12 +10,12 @@ module.exports = {
         let estaciones = new estaciones({
             localizacion: req.body.localizacion,
             nombre: req.body.nombre,
-            usuarioRegistra: req.usuario._id,
-            usuarioMantiene
+            //usuarioRegistra: req.usuario._id,
+            //usuarioMantiene
         });
         estaciones.save()
-            .then(resp => resp.populate('usuarioRegistra').execPopulate())
-            .then(resp => resp.populate('usuarioMantiene').execPopulate())
+            //.then(resp => resp.populate('usuarioRegistra').execPopulate())
+            //.then(resp => resp.populate('usuarioMantiene').execPopulate())
             .then(resp => res.status(201).json(resp))
             .catch(err => res.send(500).json(err.message))
     }
